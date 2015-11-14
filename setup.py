307788de6f13a1f3cce7422f6ea6f7329da2f956
Yoki <ispeedly@gmail.com>
2015-11-14 14:02:08 +0800

    release version 0.0.1

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+
+version = '0.0.1'
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='torpc',
+      version=version,
+      description='A tiny async tcp and duplex rpc implementation using Tornado IOLoop.',
+      url='http://github.com/yoki123/torpc',
+      download_url='',
+      long_description=read('README.md'),
+      author='Yoki',
+      author_email='ispeedly@gmail.com',
+      keywords=['tornado', 'rpc', 'async'],
+      license='MIT',
+      packages=['torpc']
+)
+
